558d1c3f00363164eb6cbf888db5865233db8a55
achapur <achapur@palaceresorts.com>
2022-07-01 17:55:29 -0400

    models sin externals

--- models/User.model.js
+++ models/User.model.js
@@ -3,15 +3,44 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+  first_name: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true,
+      required: true
     },
-    password: String,
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    profile_pic: {
+      type: String,
+      default:"https://res.cloudinary.com/dhgfid3ej/image/upload/v1558806705/asdsadsa_iysw1l.jpg"
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "STAFF", "USER"],
+      default: "USER"
+    },
+    followers: {
+        type:ObjectID,
+        Ref:"User"
+    },
+    following: {
+      type:ObjectiID,
+      Ref:"User"
+    },
+    timestamps: true
   }
 );
 
