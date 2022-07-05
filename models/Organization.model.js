5a27b50e51ba15a206aec7eb450df4135f037c6a
ana luz martinez <almadigital.mx@gmail.com>
2022-07-04 17:56:47 -0700

    updated organization model

---
+++ models/Organization.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const organizationSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    logo: {
+      type: String,
+      required: true
+    },
+    slogan:{ type: String},
+    main_language: {
+      type: String,
+      enum:["Bulgarian", "Croatian", "Cezch","Danish", "Dutch", "English","Estonian", "Finnish", "German", "Greek","Hungarian", "Irish", "Italian", "Latvian", "Maltese", "Polish", "Portuguese", "Romanian", "Slovak", "Slovenian","Spanish", "Swedish"],
+      default:"English"
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    timestamps: true
+}
+);
+
+const   Organization = model("Organization", userSchema);
+
+module.exports = Organization;
